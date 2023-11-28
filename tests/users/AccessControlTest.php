b0c92e577d719cfbc7a0d9be386047f635de0863
KarthikReddyPanyala <145713563+KarthikReddyPanyala@users.noreply.github.com>
2023-11-28 13:00:29 -0600

    Update AccessControlTest.php

--- tests/users/AccessControlTest.php
+++ tests/users/AccessControlTest.php
@@ -3,16 +3,18 @@
 class AccessControlTest extends PHPUnit\Framework\TestCase
 {
     public function testLoginPageRedirectionForNonLoggedInUsers()
-{
-    // Arrange
-    $_SESSION['vpmsuid'] = '';
+    {
+        // Arrange
+        $_SESSION['vpmsuid'] = '';
 
-    // Act
-    ob_start();
-    include(__DIR__ . '/../../users/add-vehicle.php');
-    $output = ob_get_clean();
+        // Act
+        ob_start();
+        session_start(); // Start the session
+        session_write_close(); // Close the session immediately
+        include(__DIR__ . '/../../users/add-vehicle.php');
+        $output = ob_get_clean();
 
-    // Assert
-    $this->assertContains('location:logout.php', $output);
-}
+        // Assert
+        $this->assertContains('location:logout.php', $output);
+    }
 }
