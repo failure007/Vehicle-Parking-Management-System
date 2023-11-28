92e8ee98881e43a637c423830b4508caa59f6d95
failure007 <77380374+failure007@users.noreply.github.com>
2023-11-28 00:34:48 -0600

    Update AccessControlTest.php

--- tests/admin/AccessControlTest.php
+++ tests/admin/AccessControlTest.php
@@ -1,18 +1,22 @@
-<?php
-
 class AccessControlTest extends PHPUnit\Framework\TestCase
 {
     public function testLoginPageRedirectionForNonLoggedInUsers()
-{
-    // Arrange
-    $_SESSION['vpmsaid'] = '';
+    {
+        // Arrange
+        $_SESSION['vpmsaid'] = '';
 
-    // Act
-    ob_start();
-    include('add-vehicle.php');
-    $output = ob_get_clean();
+        // Start session for the specific test case
+        session_start();
 
-    // Assert
-    $this->assertContains('location:logout.php', $output);
-}
+        // Act
+        ob_start();
+        include('add-vehicle.php');
+        $output = ob_get_clean();
+
+        // Close the session to prevent "headers already sent" issue
+        session_write_close();
+
+        // Assert
+        $this->assertContains('location:logout.php', $output);
+    }
 }
