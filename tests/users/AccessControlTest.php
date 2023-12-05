8c056b70696e7dbf87bf08c4c1868a8b611b4459
KarthikReddyPanyala <145713563+KarthikReddyPanyala@users.noreply.github.com>
2023-12-05 12:35:20 -0600

    Update AccessControlTest.php

--- tests/users/AccessControlTest.php
+++ tests/users/AccessControlTest.php
@@ -8,9 +8,8 @@ class AccessControlTest extends PHPUnit\Framework\TestCase
         $_SESSION['vpmsuid'] = '';
 
         // Act
-        ob_start();
         include(__DIR__ . '/../../users/add-vehicle.php');
-        $output = ob_get_clean();
+        
 
         // Assert
         $this->assertContains('location:logout.php', $output);
